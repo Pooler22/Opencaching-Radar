2a4dbd1a58aca0b037b8d92826b08a9b5ba8bce2
pooler22 <rudypawel12@gmail.com>
2017-03-03 20:09:34 +0100

    clean up project code

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+import './script';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Opencaching Radar</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
